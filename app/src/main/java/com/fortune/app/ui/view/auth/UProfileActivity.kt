def74fd27eceb7a5f1deb54b0733010f5fc9382f
Iyan Sanchez <145293920+S4nchzz@users.noreply.github.com>
2025-03-16 22:36:48 +0100

    Se ha añadido un dialogLogin a la hora de crear el perfil

--- app/src/main/java/com/fortune/app/ui/view/auth/UProfileActivity.kt
+++ app/src/main/java/com/fortune/app/ui/view/auth/UProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.fortune.app.ui.view.auth
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -8,6 +10,7 @@ import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -23,6 +26,8 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class UProfileActivity : AppCompatActivity() {
+    private lateinit var loadingDialog: AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -49,8 +54,16 @@ class UProfileActivity : AppCompatActivity() {
             val uProfileDTO = UProfileDTO(name, address, phone)
             val userDTO = intent.getSerializableExtra("userDTO", UserDTO::class.java)
 
+            loadingDialog = AlertDialog.Builder(this@UProfileActivity)
+                .setView(R.layout.dialog_loading)
+                .create()
+            loadingDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            loadingDialog.setCancelable(false)
+            loadingDialog.show()
+
             authViewmodel.register.observe(this) {
                 uProfileViewModel.profile.observe(this) {
+                    loadingDialog.dismiss()
                     AccountSuccesfullyCreated_Dialog().show(supportFragmentManager, "Account succesfully created")
                 }
 
