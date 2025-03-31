5061c91987892a0d6a175437a93546d84dcf36fc
Iyan Sanchez <145293920+S4nchzz@users.noreply.github.com>
2025-03-31 19:44:11 +0200

    Se ha eliminado beans providers innecesarios que hacian referencia a Room en el AppModule

--- app/src/main/java/com/fortune/app/di/AppModule.kt
+++ app/src/main/java/com/fortune/app/di/AppModule.kt
@@ -3,11 +3,6 @@ package com.fortune.app.di
 import android.content.Context
 import androidx.room.Room
 import com.fortune.app.BuildConfig
-import com.fortune.app.data.config.db.AppDatabase
-import com.fortune.app.data.repositories.db.bank_data.AccountDBRepositoryImpl
-import com.fortune.app.data.repositories.db.bank_data.CardDBRepositoryImpl
-import com.fortune.app.data.repositories.db.user.UProfileDBRepositoryImpl
-import com.fortune.app.data.repositories.db.user.UserDBRepositoryImpl
 import com.fortune.app.data.repositories.api.bank_data.AccountAPIRepositoryImpl
 import com.fortune.app.data.repositories.api.bank_data.CardAPIRepositoryImpl
 import com.fortune.app.data.repositories.api.user.UProfileAPIRepositoryImpl
@@ -35,20 +30,8 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
-        return Room.databaseBuilder(context, AppDatabase::class.java, "fortune_dbl").build()
-    }
-
-    @Provides
-    @Singleton
     fun provideUserAPIRepository(retrofit: Retrofit): UserAPIRepositoryImpl {
         return UserAPIRepositoryImpl(retrofit)
-    }
-
-    @Provides
-    @Singleton
-    fun provideUserDBRepository(appDatabase: AppDatabase): UserDBRepositoryImpl {
-        return UserDBRepositoryImpl(appDatabase)
     }
 
     @Provides
@@ -59,31 +42,14 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideUProfileDBRepository(appDatabase: AppDatabase): UProfileDBRepositoryImpl {
-        return UProfileDBRepositoryImpl(appDatabase)
-    }
-
-    @Provides
-    @Singleton
     fun provideCardAPIRepository(retrofit: Retrofit): CardAPIRepositoryImpl {
         return CardAPIRepositoryImpl(retrofit)
     }
 
-    @Provides
-    @Singleton
-    fun provideCardDBRepository(appDatabase: AppDatabase): CardDBRepositoryImpl {
-        return CardDBRepositoryImpl(appDatabase)
-    }
 
     @Provides
     @Singleton
     fun provideAccountAPIRepository(retrofit: Retrofit): AccountAPIRepositoryImpl {
         return AccountAPIRepositoryImpl(retrofit)
-    }
-
-    @Provides
-    @Singleton
-    fun provideAccountDBRepository(appDatabase: AppDatabase): AccountDBRepositoryImpl {
-        return AccountDBRepositoryImpl(appDatabase)
     }
 }
