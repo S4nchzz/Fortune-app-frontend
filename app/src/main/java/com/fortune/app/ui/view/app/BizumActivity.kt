58fba7a7254cf076cafd4dcb0c04d50829aeea7e
Iyan Sanchez <145293920+S4nchzz@users.noreply.github.com>
2025-05-09 12:44:26 +0200

    Layout de bizum creado

---
+++ app/src/main/java/com/fortune/app/ui/view/app/BizumActivity.kt
@@ -0,0 +1,26 @@
+package com.fortune.app.ui.view.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fortune.app.R
+
+class BizumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bizum)
+        adjustScreenInsets()
+
+    }
+
+    private fun adjustScreenInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
